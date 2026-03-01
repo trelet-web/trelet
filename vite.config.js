0dbaa3c75047054b6c49a44a9afd0dec4e264cc9
Faisal-8898 <faaisal8898@gmail.com>
2026-03-01 12:32:14 +0600

    a

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/trelet-web/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
